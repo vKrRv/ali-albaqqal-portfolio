58bfa689345d49a8d689bf2f8a32e477d0bf475e
Ali Albaqqal <alawi-522@hotmail.com>
2026-02-22 20:35:26 +0300

    chore: setup database connection configuration via pg pool

---
+++ backend/src/config/db.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Required for Neon cloud connections
+  }
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
