411c48d423515d33f7c56be67becf4937d1cb3fe
Ali Albaqqal <alawi-522@hotmail.com>
2026-03-01 02:45:19 +0300

    style(layout): implement global dark theme background and text colors

--- frontend/src/components/public/MainLayout.jsx
+++ frontend/src/components/public/MainLayout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router';
 const MainLayout = () => {
 
     return (
-        <div className='flex flex-col min-h-screen'>
+        <div className='flex flex-col min-h-screen bg-slate-950 text-slate-200'>
             <Navbar />
             <main className='flex-grow'>
                 <Outlet />
